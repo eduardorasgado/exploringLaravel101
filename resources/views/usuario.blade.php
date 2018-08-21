a31319ab525a4790f212bb227b538cca3fee39d2
lal <eduardo.rasgado@hotmail.com>
2018-08-21 03:30:45 -0500

    params to route web.php review

---
+++ resources/views/usuario.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Usuario</title>
+</head>
+<body>
+	Mi nombre es: {{ $nombre }}
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,17 @@ Route::match(['get', 'post'], '/contacto', function () {
 	return view('contacto');
 });
 
+// pasar parametro a la funcion de callback
+Route::get('/usuario/{nombre}', function ($nombre) {
+	// podemos pasarle los parametros obtenidos de la url
+	return view('usuario', [
+		"nombre" => $nombre
+	]);
+});
+
+/*
+Route::any('/contacto', function () {
+	return view('contacto');
+});
+
+*/
